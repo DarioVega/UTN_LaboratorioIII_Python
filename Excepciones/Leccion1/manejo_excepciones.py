0d1e914a1710af8da5b160bec39db0884f87ac8a
Darío Vega <sergiodariovega@gmail.com>
2023-04-13 21:00:02 -0300

    Creo una clase con mi propia exception

--- Excepciones/Leccion1/manejo_excepciones.py
+++ Excepciones/Leccion1/manejo_excepciones.py
@@ -1,8 +1,14 @@
+from numeros_iguales_exception import NumerosIgualesException
+
 resultado = None
-a = 10
-b = 0
 
 try:
+
+    a = int(input('Digite el primer número: '))
+    b = int(input('Digite el segundo número: '))
+
+    if a == b:
+        raise NumerosIgualesException("son iguales")
 
     resultado = a / b
 
@@ -11,12 +17,16 @@ except TypeError as e:
 
 # except Exception as e:
 except ZeroDivisionError as e:
-
     print(f"ZeroDivisionError - Ocurrió un error: {type(e)}")
 
 except Exception as e:
-
     print(f"Exception - Ocurrió un error: {type(e)}")
+
+else:
+    print("No se arrojó ninguna excepción")
+
+finally:
+    print("Ejecución del bloque finally")
 
 print(f"El resultado es: {resultado}")
 print("seguimos...")
